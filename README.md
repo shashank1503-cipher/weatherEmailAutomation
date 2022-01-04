adde93546db4848e86dfd8a671bacb4a4ea90723
Shashank Srivastava <54381338+shashank1503-cipher@users.noreply.github.com>
2022-01-04 11:45:29 +0530

    Added Readme

---
+++ README.md
@@ -0,0 +1,97 @@
+# Weather Email Automation App
+
+> Assignment Submission for Xtreme Technologies.
+
+![](https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png)
+
+## Requirements  (Prerequisites)
+
+Tools and packages required to successfully install this project.
+For example:
+
+* Python 3.6.5 and up [Install](https://www.python.org/downloads/)
+
+## Installation
+
+A step by step list of commands / guide that informs how to install an instance of this project.
+
+* Clone the project
+
+    ```git clone project-url```
+
+* Install virtualenv to create virtual environment
+
+    ```pip install virtualenv```
+
+* Create and activate virtualenv
+
+    ```virtualenv venv```
+
+    ```cd venv/scripts/activate```
+
+* Change Directory to base directory of django project
+
+    ```cd..```
+    ```cd..```
+
+* Install Requirements
+
+    ```pip install -r requirements.txt```
+
+## Screenshots
+
+Main Page to get input
+
+![Input Page](https://user-images.githubusercontent.com/54381338/148014781-ce87eadf-ca00-4104-9b10-f2ec40bc950b.png)
+
+Email Sent Confirmation Page
+
+![Email Sent Confirmation](https://user-images.githubusercontent.com/54381338/148014908-dc7c0a11-da6b-4e25-a32d-60ac1d25a708.png)
+
+Email
+
+![Email](https://user-images.githubusercontent.com/54381338/148015070-5a05255b-6e8d-4c68-a1e6-69f3ba1d7636.png)
+
+Django Admin Page
+
+![Django Admin Page](https://user-images.githubusercontent.com/54381338/148015745-e7bbb7c0-8437-4dab-af6b-fb0bc00e1995.png)
+
+Error Handling
+
+* SMTP Error
+    ![SMTP_Error](https://user-images.githubusercontent.com/54381338/148015404-ac567224-4063-49f9-85d2-edac046a34c8.png)
+* Bad Header Error
+    ![BadHeaderError](https://user-images.githubusercontent.com/54381338/148015554-2b23c585-81e2-4bdd-8c84-e22f4e5d091d.png)
+
+## Features
+
+The project uses OpenweatherMap API to get the weather data for a specific city and sends the temperature to the given mail using Django's inbuilt Email Service. Noticeable features about the codebase:
+
+* Robust Code
+* Use of Environment Variables to store secret_keys for added security advantage
+
+## Usage example
+
+The project is deployed using Heroku.
+Application can be used by visiting the following link
+[Deployment](https://weather-email-automation.herokuapp.com/)
+
+## Tech Stack / Built With
+
+1. [Django](https://www.djangoproject.com/) - Python based Framework
+
+## How to Contribute
+
+This Repository is currently not accepting any contributions.
+
+## Authors
+
+Shashank Kumar Srivastava  – shashank.srivastava25sks@gmail.com
+
+You can find me here at:
+[Github](https://github.com/shashank1503-cipher)
+[LinkedIn](https://www.linkedin.com/in/shashank-srivastava-a72899201/)
+
+## Credits
+
+This project uses [OpenWeatherMap](https://openweathermap.org/api) to fetch current weather data.
--- main/views.py
+++ main/views.py
@@ -35,35 +35,38 @@ def sendEmail(request):
     email = request.GET['email']
     #Generating API Endpoint
     apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={APIkey}'.format(cityName = city,APIkey = apiKey)
-    resp = requests.get(apiUrl)
-    #Parsing into JSON
-    weather = resp.json()
-    temperature = weather['main']['temp']
-    temperatureInC = temperature - 273 #API Returns temperature in K which is converted to C.
-    #formating time
-    cTime = datetime.datetime.now()
-    fTime = str(cTime.hour) + ":" + str(cTime.minute) + ":" + str(cTime.second) + " IST"
-    
-    #Getting Emoji
-    emoji = getEmoji(temperatureInC)
-    #Generating Mail Content
-    subject = 'Hi {name}, interested in our services.'.format(name=name)
-    message = 'Temperature in {city} is {temp:.2f} C {emoji} at {time}'.format(city=city,temp=temperatureInC,emoji=emoji,time=fTime)
-    email_from = settings.EMAIL_HOST_USER
-    recipient_list = [email, ]
-    #Sendig Mail through Django's Inbuilt Mailing System
     try:
-        send_mail( subject, message, email_from, recipient_list,fail_silently=False )
-        #Saving Client Details to Database
-        c = ClientDetails(username = name,email=email,temperature=temperatureInC,timeRequested = cTime)
-        c.save()
-        return response.HttpResponse("<p>Email Sent</p><a href='/'>Go Back</a> ")
-    except BadHeaderError:
-        return response.HttpResponse("<p>Invalid Header Error</p><a href='/'>Go Back</a> ")
-    except SMTPException as e:
-        return response.HttpResponse("<p>There was an error sending email</p><p>Error:- {error}</p><a href='/'>Go Back</a>".format(error = e))
+        resp = requests.get(apiUrl)
+        #Parsing into JSON
+        weather = resp.json()
+        temperature = weather['main']['temp']
+        temperatureInC = temperature - 273 #API Returns temperature in K which is converted to C.
+        #formating time
+        cTime = datetime.datetime.now()
+        fTime = str(cTime.hour) + ":" + str(cTime.minute) + ":" + str(cTime.second) + " IST"
+        
+        #Getting Emoji
+        emoji = getEmoji(temperatureInC)
+        #Generating Mail Content
+        subject = 'Hi {name}, interested in our services.'.format(name=name)
+        message = 'Temperature in {city} is {temp:.2f} C {emoji} at {time}'.format(city=city,temp=temperatureInC,emoji=emoji,time=fTime)
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [email, ]
+        #Sendig Mail through Django's Inbuilt Mailing System
+        try:
+            send_mail( subject, message, email_from, recipient_list,fail_silently=False )
+            #Saving Client Details to Database
+            c = ClientDetails(username = name,email=email,temperature=temperatureInC,timeRequested = cTime)
+            c.save()
+            return response.HttpResponse("<p>Email Sent</p><a href='/'>Go Back</a> ")
+        except BadHeaderError:
+            return response.HttpResponse("<p>Invalid Header Error</p><a href='/'>Go Back</a> ")
+        except SMTPException as e:
+            return response.HttpResponse("<p>There was an error sending email</p><p>Error:- {error}</p><a href='/'>Go Back</a>".format(error = e))
+        except:
+            return response.HttpResponse("<p>Could not send mail. Try Again Later</p><a href='/'>Go Back</a>")
     except:
-        return response.HttpResponse("<p>Could not send mail. Try Again Later</p><a href='/'>Go Back</a>")
+        return response.HttpResponse("<p>Error in fetching data from API</p>")
 #View to get Input
 def getInput(request):
     return render(request,'index.html')
